55bfba20d9bab13bad01913575895c826bd31ef3
bernasconi <bernasconi84@outlook.com>
2022-01-24 13:31:51 -0300

    calculdora.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Criando uma calculadora em JavaScript
+
+/* Para este desafio vamos utilizar alguns métodos nativos do JavaScript
+
+° Number() - para converter valores em números.
+° prompt() - pra registar entradas de usuários;
+° Alert()m- para mostrar mensagens para usuários;
+° Template Strings - para usar string junto co expressões;   */ 
+
+
+
